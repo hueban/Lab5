52f91f8044ebe9ac2800a3216a04483d775499aa
Johan <jsbollll@gmail.com>
2022-02-28 13:35:22 +0100

    test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new OpenEvent().doEvent();
+        new StartEvent().doEvent();
+    }
+}
+
+abstract class Event {
+    abstract void doEvent();
+}
+
+
+class StartEvent extends Event {
+
+    @Override
+    void doEvent() {
+        System.out.println("StartEvent");                
+    }
+
+}
+
+
+class OpenEvent extends StartEvent{
+
+    @Override
+    void doEvent() {
+        super.doEvent();
+        System.out.println("OpenEvent");        
     }
 }
