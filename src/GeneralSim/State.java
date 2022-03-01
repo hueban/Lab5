0b635e3cdf0ec901dc662a04b771ef4dfd264e78
Johan <jsbollll@gmail.com>
2022-03-01 19:30:05 +0100

    added basic getter/setter

--- src/GeneralSim/State.java
+++ src/GeneralSim/State.java
@@ -5,4 +5,20 @@ import java.util.Observable;
 public class State extends Observable {
     protected boolean running;
     protected float currentTime;
+
+
+    public boolean isRunning() {
+        return running;
+    }
+
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+
+    public float getCurrentTime() {
+        return currentTime;
+    }
+
 }
