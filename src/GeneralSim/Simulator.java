1333ac50dc01cc333980a30dfb8ca9af7756696c
Johan <jsbollll@gmail.com>
2022-03-01 19:29:34 +0100

    added basic constructor

--- src/GeneralSim/Simulator.java
+++ src/GeneralSim/Simulator.java
@@ -1,7 +1,13 @@
 package GeneralSim;
 
 public class Simulator {
-    Simulator(State state, EventQueue eventqueue, View view) {
-        // TODO Auto-generated method stub
+    private State state;
+    private EventQueue eventQueue;
+    private View view;
+
+    Simulator(State state, EventQueue eventQueue, View view) {
+        this.state      = state;
+        this.eventQueue = eventQueue;
+        this.view       = view;
     }
 }
